044d6d7d882bc75b076a77e581a743cd39e7870c
Jocelyn Sun <jocelynsun1@gmail.com>
2022-08-31 16:41:45 -0400

    Add jsqlparser example

--- P1/src/P1/Main.java
+++ P1/src/P1/Main.java
@@ -1,7 +1,26 @@
 package P1;
 
+import java.io.FileReader;
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.Select;
+
 public class Main {
+	private static final String queriesFile = "input/queries.sql";
+	
 	public static void main(String[] args) {
-		System.out.println("hi");
+		try {
+			CCJSqlParser parser = new CCJSqlParser(new FileReader(queriesFile));
+			Statement statement;
+			while ((statement = parser.Statement()) != null) {
+				System.out.println("Read statement: " + statement);
+				Select select = (Select) statement;
+				System.out.println("Select body is " + 
+				select.getSelectBody());
+			}
+		} catch (Exception e) {
+			System.err.println("Exception occurred during parsing");
+			e.printStackTrace();
+		}
 	}
 }
