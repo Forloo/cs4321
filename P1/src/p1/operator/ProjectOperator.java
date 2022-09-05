4cb9c9bcf56b3ceda72744a0301872376f948755
Anna Zhang <annazhang@dhcp-vl2041-3164.eduroam.cornell.edu>
2022-09-05 15:42:39 -0400

    project operator setup

---
+++ P1/src/p1/operator/ProjectOperator.java
@@ -0,0 +1,29 @@
+package p1.operator;
+
+import p1.Tuple;
+import p1.databaseCatalog.DatabaseCatalog;
+
+
+public class ProjectOperator extends Operator {
+
+	public Tuple getNextTuple() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void dump() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void dump(String outputFile) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
