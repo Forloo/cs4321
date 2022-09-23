4ef465fae509e65ded43f013e77db2374cc9b8fd
annazhang <az458@cornell.edu>
2022-09-22 23:51:55 -0400

    create physical plan builder

---
+++ P1/src/p1/util/PhysicalPlanBuilder.java
@@ -0,0 +1,38 @@
+package p1.util;
+
+import p1.logicaloperator.*;
+
+/**
+ * Walks through logical plan and builds a physical plan
+ */
+public class PhysicalPlanBuilder {
+	
+	public PhysicalPlanBuilder(LogicalPlan logicalplan) {
+		
+	}
+	
+	public void visit(LogicalUnique unique) {
+		
+	}
+	
+	public void visit(LogicalSort sort) {
+		
+	}
+	
+	public void visit(LogicalScan scan) {
+		
+	}
+	
+	public void visit(LogicalProject project) {
+		
+	}
+	
+	public void visit(LogicalJoin join) {
+		
+	}
+	
+	public void visit(LogicalFilter filter) {
+		
+	}
+	
+}
