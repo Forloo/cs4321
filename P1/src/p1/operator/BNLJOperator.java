6cc8bc1db40f8acd071287af097045743ebd44f6
hc659 <hc659@cornell.edu>
2022-10-09 22:19:42 -0400

    Finished bnlj need to test

---
+++ P1/src/p1/operator/BNLJOperator.java
@@ -0,0 +1,213 @@
+package p1.operator;
+
+import java.util.ArrayList;
+
+import net.sf.jsqlparser.expression.Expression;
+import p1.util.ExpressionEvaluator;
+import p1.util.Tuple;
+
+public class BNLJOperator extends Operator{
+
+	// The left operator for bnlj 
+	private Operator left;
+	// The right operator for bnlj 
+	private Operator right;
+	// The schema for the result table
+	private ArrayList<String> schema;
+	// The list of expression for bnlj 
+	private ArrayList<Expression> where;
+	//Keep track of where we are in the tuple for the left table of the joined column
+	private Tuple currTuple;
+	// The current right tuple value
+	private Tuple rightTuple;
+	// A string denoting the list of tables that we need for executing the given join
+	private String tables;
+	
+	// The number of tuples that we scan into a block in total
+	private int tuplePerScan;
+	// The number of bytes for a page.
+	public static final int pageSize=4096;
+	// The Arraylist of tuples containing the elements in the outer block
+	private ArrayList<Tuple> outerBlock;
+	private int outerPos;
+	private boolean lastBlock;
+	
+	/**
+	 * The constructor for bnlj
+	 * @param tables The string delimited by commas that provided the tables needed for the join
+	 * @param left The left child of the bnlj 
+	 * @param right The right child of the bnlj
+	 * @param exp The list of expressions imposed on the result tuples of the bnlj
+	 * @param bufferSize The number of pages that will be used for the outer loops blocks
+	 */
+	public BNLJOperator(String tables,Operator left, Operator right, ArrayList<Expression> exp, int bufferSize) {
+		this.tables=tables;
+		this.left=left;
+		this.right=right;
+		where=exp;
+		
+		ArrayList<String> schema2= new ArrayList<String>();
+		schema2.addAll(left.getSchema());
+		schema2.addAll(right.getSchema());
+		schema=schema2;
+		
+		// The size of the left table is how long the schema is 
+		int leftTuplelength= left.getSchema().size();
+		// Subtract 8 since each page has metadata that takes up 8 bytes
+		tuplePerScan= bufferSize*((pageSize-8)*(4*leftTuplelength));
+		
+		// Get the block from the child
+		outerBlock=this.getBlock();
+		outerPos=0;
+	}
+	
+	
+	/**
+	 * Retrieves the next tuple for bnlj
+	 */
+	public Tuple getNextTuple() {
+		// For each block we read the inner table once meaning that we want to
+		// reset the outer table to the index one instead of resetting the inner
+		// table and doing multiple page reads in that instance.
+		
+		while(true) {
+			
+			// Check if the index for the outer block is in range
+			if (outerPos>=outerBlock.size()) {
+				// Get the next right tuple if that value is not null
+				// then that means that we need to reset the outer tuple 
+				// back to the beginning 
+				rightTuple=right.getNextTuple();
+				// If the right tuple is null then it means that we are done
+				// with this current outer block
+				if (rightTuple==null) {
+					if (lastBlock) {
+						// If this is the last block then there is no next tuple
+						return null;
+					}
+					// Otherwise if it not the last block get the next block
+					outerBlock=this.getBlock();
+					
+					// If we reset the outerblock then we need to reset the outerpos 
+					outerPos=0;
+				}
+				// If the righttuple is not null then we still have other tuples
+				// on the outer table to iterate through
+				else {
+					outerPos=0;
+				}
+			}
+			
+			// The left tuple value here is the one in the arraylist for the outerblock
+			Tuple leftSide= outerBlock.get(outerPos);
+			// The value of the right tuple is just hte current right tuple
+			Tuple currRight= rightTuple;
+			
+			// Make them into one larger tuple
+			ArrayList<String> together= new ArrayList<String>();
+			together.addAll(leftSide.getTuple());
+			together.addAll(currRight.getTuple());
+			
+			// Make the new tuple
+			Tuple combined= new Tuple(together);
+			
+			if (where==null) {
+				return combined;
+			}
+			else {
+				ExpressionEvaluator eval = new ExpressionEvaluator(combined, schema);
+				// There must be at least one expression if we enter this loop
+				boolean allTrue = true;
+				for (int i = 0; i < this.getWhere().size(); i++) {
+					this.getWhere().get(i).accept(eval);
+					allTrue = allTrue && (Boolean.parseBoolean(eval.getValue()));
+				}
+				if (allTrue) {
+					return combined;
+				}
+			}
+			
+		}
+	}
+	
+	/**
+	 * Retrieves the conditions for the join
+	 * @return An arraylist of all the expression for bnlj
+	 */
+	public ArrayList<Expression> getWhere(){
+		return where;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reset(int idx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<String> getSchema() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTable() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void dump() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dump(String outputFile) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * A method to get the outer block for bnlj
+	 * @return A list of tuples for the outer block.
+	 */
+	private ArrayList<Tuple> getBlock(){
+		int tuplesToScan=tuplePerScan;
+		ArrayList<Tuple> outer= new ArrayList<Tuple>();
+		
+		// Check if there is a placeholder element for the last tuple that we scanned on
+		// left child but there was no room left in the last buffer
+		if (currTuple!=null) {
+			tuplesToScan=tuplesToScan-1;
+			outer.add(currTuple);
+		}
+		// Get the next tuple for the left table
+		Tuple leftTuple=left.getNextTuple();
+		while (tuplesToScan>0 && leftTuple!=null) {
+			tuplesToScan=tuplesToScan-1;
+			outer.add(leftTuple);
+			leftTuple=left.getNextTuple();
+		}
+		
+		// Left tuple not null then keep track of this tuple as the next start tuple
+		if (leftTuple!=null) {
+			currTuple=leftTuple;
+		}
+		
+		// If there are remaining tuples when we get here then that means that 
+		// this must be the last block
+		if (tuplesToScan>0) {
+			lastBlock=true;
+		}
+		
+		return outer;
+	}
+
+}
---
+++ P1/src/test/BNLJOperatorTest.java
@@ -0,0 +1,57 @@
+package test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.statement.Statement;
+import p1.util.DatabaseCatalog;
+
+public class BNLJOperatorTest {
+	String queriesFile = "input" + File.separator + "queries.sql";
+	String queriesOutput = "output";
+	String dataDir = "input" + File.separator + "db" + File.separator;
+
+	// Get the file list containing all file inputs
+	File inputDir = new File(dataDir + "data");
+	String[] allFiles = inputDir.list();
+	File[] fileList = new File[allFiles.length];
+	File schema = new File(dataDir + "schema.txt");
+	String tempDir = "././temp";
+	File configFile = new File("././input/plan_builder_config.txt");
+	
+	@Test
+	public void bnljTest() {
+		for (int i = 0; i < allFiles.length; i++) {
+			File file = new File(dataDir + "data" + File.separator + allFiles[i]);
+			fileList[i] = file;
+		}
+
+		DatabaseCatalog.getInstance(fileList, schema, configFile, tempDir);
+
+		ArrayList<Statement> queries = new ArrayList<Statement>();
+
+		// Parse all of the queries and put them into the arraylist.
+
+		try {
+			CCJSqlParser parser = new CCJSqlParser(new FileReader("././input/queries.sql"));
+			Statement statement;
+			int queryCount = 1;
+			while ((statement = parser.Statement()) != null) {
+				try {
+					// Parse statement
+					queries.add(statement);
+				} catch (Exception e) {
+					System.err.println("Exception occurred during query " + queryCount);
+					e.printStackTrace();
+				}
+				queryCount++;
+			}
+		} catch (Exception err) {
+			System.out.println("The file you are looking for was not found");
+		}
+	}
+}
