91ce12fcab4a47c6d0c7bb1992b1eeccb68f12e8
forloo <kiplingpower6@gmail.com>
2022-10-16 13:58:33 -0400

    intermediate implementaiton of merge

--- P1/src/p1/operator/ExternalSortOperator.java
+++ P1/src/p1/operator/ExternalSortOperator.java
@@ -1,5 +1,6 @@
 package p1.operator;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,12 +31,14 @@ public class ExternalSortOperator extends Operator {
 	 * @param op     the child operator
 	 * @param orders the sorting order
 	 * @param bufferSize The number of pages that will be used 
+	 * @param tempDir the file path for temporary directory
 	 */
-	public ExternalSortOperator(Operator op, ArrayList<String> orders, int bufferSize) {
+	public ExternalSortOperator(Operator op, ArrayList<String> orders, int bufferSize, String tempDirPath) {
 		child = op;
 		schema = child.getSchema();
 		bufferPages = bufferSize;
 		order = orders;
+		tempDir = tempDirPath;
 		
 		// Get the indices of columns to order by
 		for (String col : order) {
@@ -45,9 +48,11 @@ public class ExternalSortOperator extends Operator {
 	
 	/**
 	 * Set name of temp file for each run
+	 * @param pass
+	 * @param run
 	 */
 	public String nameTempFile(int pass, int run) {
-		return "Pass_" + Integer.toString(pass) + "Run_" + Integer.toString(run);
+		return tempDir + "Pass_" + Integer.toString(pass) + "Run_" + Integer.toString(run);
 	}
 
 	/**
@@ -83,7 +88,7 @@ public class ExternalSortOperator extends Operator {
 			
 	        run++;
 			
-	        merge(run, bufferPages, writer, fileList, tuplesPerPage);		
+	        merge(run, bufferPages, fileList, tuplesPerPage);		
 		}	
 	}
 	
@@ -93,56 +98,102 @@ public class ExternalSortOperator extends Operator {
 	 * Merge the runs
 	 * @param n is the number of sorted runs left to merge into one big run
 	 * @param b is the size of buffer
+	 * @param fileList is the list of file names in temp directory to use
+	 * @param tuplesPerPage represents the number of tuples that fills the output buffer for each step of merge.
 	 */
-	public void merge(int n, int b, String temp, List<String> fileList,int tuplesPerPage) {
+	public void merge(int n, int b, List<String> fileList,int tuplesPerPage) {
+		
+		//are the pages all full?
+		//do we always have even number of runs to merge?
+		//change file dir to read from 
+		//clean temp directory between queries
+		
 		int outDirSize = fileList.size() * 2;
-		tuplesPerPage = tuplesPerPage / 2;//divide so while loop below works
+		tuplesPerPage = tuplesPerPage / 2;//this is for output buffer
 		while(outDirSize != 1) { //initialize before merge step
 			//initialize possible runs to go in input buffer
+			outDirSize = outDirSize / 2;
+			//adjust fileList here
+			List<String> subItems = new ArrayList<String>(fileList.subList(0, outDirSize)); //change
 			ArrayList<BinaryTupleReader> fileReaders = new ArrayList<BinaryTupleReader>();
-			for (String f : fileList) {
+			for (String f : subItems) {
 				//make BinaryTupleReader for all runs...
 				fileReaders.add(new BinaryTupleReader(f));
 			}
 			
 			//get the smallest tuple out of each run in input buffer
 			ArrayList<Tuple> bMinusOneTuple = new ArrayList<Tuple>();
+			int numTupInBuff = 0;
 			for (int i=0;i<b-1;i++) {//add b-1 tuples to find the min
 				if(fileReaders.get(i).nextTuple() == null) {
 					fileReaders.remove(i);
 				} 
+				if(fileReaders.isEmpty()) {//constant time operation, takes care of edge case
+					break;
+				}
 				bMinusOneTuple.add(fileReaders.get(i).nextTuple());
+				numTupInBuff++;
 			}
 			int outBufferNumTup = 0;
 			//QUESTION: What is the size of the intermediate run? - twice the original...
 			tuplesPerPage = tuplesPerPage * 2;
 			int bwOutDirSize = 0;
-			outDirSize = outDirSize / 2;
+			
+			if (outDirSize == 1) {//included here bc once putting under while loop below, code doesn't reach
+				break;
+			}
+			
+			int ms = 0;
+			int rn = 0;
+//			ArrayList<String> interFileList = new ArrayList<String>();
 			ArrayList<Tuple> outputBuffer = new ArrayList<Tuple>();
 			while(bwOutDirSize != outDirSize) { //merging 1 step (stop merge when outDirSize number of 
 				//runs in output temp directory)
 				//find the minTup among the tuples added
 				Tuple minTup = null;
 				CompareTuples tc = new CompareTuples();
+				int minTupIndx = 0;
+				int inc = 0;
 				for(Tuple tup : bMinusOneTuple) {
 					if (minTup == null || tc.compare(tup,minTup) == -1){
 						minTup = tup;
+						minTupIndx = inc;
 					}
+					inc++;
 				}
+				//my file reader adds up to bMinusOneTuple
+				//add min to output buffer and "remove" it from input buffer
 				outputBuffer.add(minTup);
 				outBufferNumTup++;
+				if (fileReaders.get(minTupIndx).nextTuple() == null) {
+					if (fileReaders.get(numTupInBuff + 1) != null) {
+						fileReaders.get(numTupInBuff+1); //get the next fileReader not read to the right... and set to minTupIndx
+						fileReaders.set(minTupIndx, fileReaders.get(numTupInBuff+1));
+						fileReaders.remove(numTupInBuff+1);//delete that one
+					} else {//if no other runs to put in input buffer, just remove them
+						fileReaders.remove(minTupIndx); 
+						bMinusOneTuple.remove(minTupIndx);
+					}
+				} else{ //else just keep getting next tuple from the same run
+					bMinusOneTuple.set(minTupIndx, fileReaders.get(minTupIndx).nextTuple());
+				}
 				
-				if (outBufferNumTup == tuplesPerPage) { //do we have to create files whenever the 
+				
+				if (outBufferNumTup == tuplesPerPage || fileReaders.isEmpty()) { //do we have to create files whenever the 
 					//output buffer is full? slide says to write to disk when output buffer is full
 					//but I want each files to be intermediate runs, two runs combined, but if i 
 					//output whenever output buffer is full, the files will only contain one page, not 
 					//runs combined...
-					BinaryTupleWriter writer = new BinaryTupleWriter("???");
+					String fileName = tempDir + File.separator + "mergeStep_" + (ms) + "_run_" + (rn);
+					fileList.set(bwOutDirSize-1, fileName); //overwrite fileList and get first n elements next merge
 					bwOutDirSize ++;
+					ms++;
+					rn++;
+					BinaryTupleWriter writer = new BinaryTupleWriter(fileName);
 					outputBuffer = new ArrayList<Tuple>();//reset output buffer
 					for(Tuple t : outputBuffer) {
 					    writer.writeTuple(t);
-					}
+					} //does this pad the binary file with zeros?
 					writer.close();
 				}
 			}
